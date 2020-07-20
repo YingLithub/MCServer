c4589474653cef743960f23d6cfd4d76c0b70323
jason920612 <30766214+jason920612@users.noreply.github.com>
2020-07-20 16:14:42 +0800

    移除不必要的code和使用goto設定迴圈

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
 
+:a
 #腳本開始
 echo echo "請記得使用java 11"
 rm paperlatest.jar #自動更新最新版開始
 curl -o paperlatest.jar https://papermc.io/api/v1/paper/1.16.1/latest/download #自動更新最新版結束
 java -Xms8G -Xmx8G -Dfile.encoding=UTF-8 -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:-OmitStackTraceInFastThrow -XX:+AlwaysPreTouch  -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=8 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=true -Daikars.new.flags=true -jar paperlatest.jar nogui
 #無限循環執行
-./start.sh
+goto a
